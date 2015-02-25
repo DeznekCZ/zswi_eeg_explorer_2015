32161dc9f0ce41cad41a61aa5f6536d1cb1cb8f4
seky739 <seky739@seznam.cz>
2015-02-25 20:38:43 +0100

    Ahoj takže jsem jen doplnil metodu čtení binary(*.eeg) po bytech obávám se ale že bude potřebovat ještě úpravy protože při velkých velikostech může dojít k poměrně dlouhému načítání. Avšak po jednom načtení budeme mít data v poli bytu. tzn nebude nutné je znovu načítat.

--- ZSWI_EEG_EXPLORER/src/cz/eeg/data/EEG.java
+++ ZSWI_EEG_EXPLORER/src/cz/eeg/data/EEG.java
@@ -1,11 +1,53 @@
 package cz.eeg.data;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import org.omg.CORBA.portable.ValueBase;
 
 public class EEG extends File{
 
+	/**pole reprezentujici data v danem souboru*/
+	private static byte [] dat;
+	
 	public EEG(String name) {
 		super(name);
+		ctiByte(name);
 	}
 
+	
+	/**Getter pro bytove pole dat*/
+	public static byte[] getDat() {
+		return dat;
+	}
+
+
+
+	/**
+	 * Metoda naplneni pole byte daty 
+	 * @param name Jmeno cteneho datoveho souboru*/
+	public static void ctiByte(String name)
+	{      
+		try
+		{
+			RandomAccessFile data = new RandomAccessFile(name,"rw");
+			long size = data.length();
+			dat= new byte [(int)data.length()];
+			for (int x=0; x < size; x++)
+			{
+				//data.seek(x);
+				//byte b = data.readByte();
+				dat[x]=data.readByte();
+				//System.out.print(b+" ");
+				   
+			}
+			//System.out.println();
+			data.close();
+		}
+		catch (IOException ex)
+		{ }
+	}
+	
+	
 }

d8504ec1c17aafa55f6c860d01a01eaa56d6e1cf
seky739 <seky739@seznam.cz>
2015-03-04 09:00:03 +0100

    Prvotni sestaveni VHDR objektu

--- ZSWI_EEG_EXPLORER/src/cz/eeg/data/vhdrmerge/Vhdr.java
+++ ZSWI_EEG_EXPLORER/src/cz/eeg/data/vhdrmerge/Vhdr.java
@@ -5,71 +5,126 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-
-public class Vhdr {
-	private static String dataFormat;
-	private static String dataOrient;
-	private static int numberOfChannels;
-	private static int samplingInterval;
-	private static String binaryFormat;
-	private static String popis2=null;
-	private static String popis1=null;
-	private static String popis3=null;
-	private static Channel[] kanaly;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 
 
+public class Vhdr extends JSplitPane {
+	
+
+	private String dataFormat;
+	private String dataOrient;
+	private int numberOfChannels;
+	private int samplingInterval;
+	private String binaryFormat;
+	private String dataFile;
+	private String markerFile;
+	private String codePage;
+	private String popis2=null;
+	private String popis1=null;
+	private String popis3=null;
+	private Channel[] kanaly;
 
 
-	public static String getPopis3() {
-		return popis3;
+	private JTextArea input;
+	private JTextArea markerTable;
+	
+	private String projdiKanaly(){
+		String s="";
+		for(int i=0;i<kanaly.length;i++){
+			s+=kanaly[i].toString()+"\n";
+		}
+		return s;
+	}
+	
+	public String vhdr(){
+		return new StringBuilder()
+					.append("[Common Infos]\n")
+					.append("Codepage="+getCodePage()+"\n")
+					.append("DataFile="+dataFile+"\n")
+					.append("MarkerFile="+markerFile+"\n")
+					.append("DataFormat="+dataFormat+"\n")
+					.append(popis1+"\n")
+					.append("DataOrientation="+dataOrient+"\n")
+					.append("NumberOfChannels="+numberOfChannels+"\n")
+					.append(popis3+"\n")
+					.append("SamplingInterval="+samplingInterval+"\n")
+					.append("\n")
+					.append("[Binary Infos]\n")
+					.append("BinaryFormat="+binaryFormat+"\n")
+					.append("\n")
+					.append("[Channel Infos]\n")
+					.append(popis2)
+					.append(projdiKanaly())
+				.toString();
 	}
 
-
-	public static String getDataFormat() {
+	public String getDataFormat() {
 		return dataFormat;
 	}
 
 
-	public static String getDataOrient() {
+	public String getDataOrient() {
 		return dataOrient;
 	}
 
 
-	public static int getNumberOfChannels() {
+	public int getNumberOfChannels() {
 		return numberOfChannels;
 	}
 
 
-	public static int getSamplingInterval() {
+	public int getSamplingInterval() {
 		return samplingInterval;
 	}
 
 
-	public static String getBinaryFormat() {
+	public String getBinaryFormat() {
 		return binaryFormat;
 	}
 
-	public static String getPopis2() {
+
+	public String getDataFile() {
+		return dataFile;
+	}
+
+
+	public String getMarkerFile() {
+		return markerFile;
+	}
+
+
+	public String getCodePage() {
+		return codePage;
+	}
+
+
+	public String getPopis2() {
 		return popis2;
 	}
 
 
-	public static String getPopis1() {
+	public String getPopis1() {
 		return popis1;
 	}
 
 
-	public static Channel[] getKanaly() {
+	public String getPopis3() {
+		return popis3;
+	}
+
+
+	public Channel[] getKanaly() {
 		return kanaly;
 	}
 
-	
+
 	public Vhdr(String vstup){
 		cti(vstup);
 	}
 	
 
-	public static void cti(String file){
+	public void cti(String file){
 		try{
 			InputStream ips=new FileInputStream(file); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
@@ -77,9 +132,9 @@ public class Vhdr {
 			String line;
 			while ((line=br.readLine())!=null){
 				if(line.equals("[Common Infos]")){
-					int pocet=0;
-					while(pocet<3){br.readLine();pocet+=1;}
-
+					codePage=br.readLine().split("=")[1];
+					dataFile=br.readLine().split("=")[1];
+					markerFile=br.readLine().split("=")[1];
 					dataFormat=br.readLine().split("=")[1];
 					popis1=br.readLine();
 					dataOrient=br.readLine().split("=")[1];

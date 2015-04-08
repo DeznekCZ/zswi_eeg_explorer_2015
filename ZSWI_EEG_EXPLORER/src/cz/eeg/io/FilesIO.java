b4164d7aabf86aa2a9a99b2de2702ed042efa75d
DeznekCZ <peugeot.55@seznam.cz>
2015-04-08 22:40:50 +0200

    Příprava pro merge

--- ZSWI_EEG_EXPLORER/src/cz/eeg/io/FilesIO.java
+++ ZSWI_EEG_EXPLORER/src/cz/eeg/io/FilesIO.java
@@ -2,16 +2,23 @@ package cz.eeg.io;
 
 import java.io.File;
 
+import cz.eeg.data.DATA;
 import cz.eeg.data.Vhdr;
 
 public class FilesIO {
-	public boolean isReadable(File vhdrPath){
+	public boolean isReadable(File vhdrPath) {
 		return false;
 	}
-	public Vhdr read(File vhdrPath){
+	public Vhdr read(File vhdrPath) {
 		return new Vhdr();
 	}
-	public boolean write(Vhdr linkedVhdr){
+	public boolean write(Vhdr linkedVhdr) {
 		return false;
+	}
+	public File backupDataFile(File dataFile) {
+		return dataFile;
+	}
+	public Vhdr mergeVhdrs(Vhdr... vhdrInstances) throws IOArrayException {
+		return new Vhdr();
 	}
 }

eebdbe3df7f903cdb74ebc8f663771a76cde8bc2
DeznekCZ <peugeot.55@seznam.cz>
2015-05-06 22:06:22 +0200

    Chybějící třída MergeItem

---
+++ ZSWI_EEG_EXPLORER/src/cz/eeg/ui/fileeditor/MergeItem.java
@@ -0,0 +1,39 @@
+package cz.eeg.ui.fileeditor;
+
+import static cz.deznekcz.tool.Lang.LANG;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JMenuItem;
+
+import cz.eeg.data.EegFile;
+import cz.eeg.io.FileReadingException;
+import cz.eeg.io.FilesIO;
+import cz.eeg.ui.GuiManager;
+import cz.eeg.ui.dialog.DialogManagement;
+
+public class MergeItem {
+
+	public static JMenuItem from(final EegFile target, final EegFile source) {
+		JMenuItem menuItem = new JMenuItem(source.getName());
+		menuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					GuiManager.EDITOR.open(FilesIO.mergeTMP(target, source));
+				} catch (IOException | FileReadingException e1) {
+					DialogManagement.open(DialogManagement.ERROR, e1.getMessage());
+				}
+			}
+		});
+		return menuItem;
+	}
+
+	public static JMenuItem voidItem() {
+		return new JMenuItem(LANG("merge_no_mergeables"));
+	}
+
+}
